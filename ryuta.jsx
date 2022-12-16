2a05fa7e492156242a97d4d64cebbd5428eb6fa6
seo-y <hachi10@naver.com>
2022-12-17 00:46:51 +0900

    ryuta

---
+++ ryuta.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {css} from @emotion/react
+
+export class ryuta extends Component {
+  render() {
+    return (
+      <div>ryuta</div>
+    )
+  }
+}
+
+export default ryuta
